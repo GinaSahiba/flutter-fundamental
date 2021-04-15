e955edd090f66d7284b23859554fb2b9349f20ce
Gina <ginasahiba@gmail.com>
2021-04-15 21:12:43 +0700

    Media Query

---
+++ demo_project/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:demo_project/main_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
