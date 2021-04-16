a69390d0fb5c6a31a30fb2b96f09d27e87ec5488
Gina <ginasahiba@gmail.com>
2021-04-16 23:07:47 +0700

    BLoC State Management(flutter_bloc package)

---
+++ bloc_blocflutterpackage/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bloc_blocflutterpackage/color_bloc.dart';
+import 'package:bloc_blocflutterpackage/main_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider<ColorBloc>(
+          builder: (context) => ColorBloc(), child: MainPage()),
+    );
+  }
+}
